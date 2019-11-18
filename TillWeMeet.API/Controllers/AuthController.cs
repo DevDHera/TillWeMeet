e17d52f5a4862d4a4ad27a769e7c258f8e8a092e
Devin <devdhera@gmail.com>
2019-11-18 11:39:49 +0530

    Reconfigured login object to include user info

--- TillWeMeet.API/Controllers/AuthController.cs
+++ TillWeMeet.API/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
@@ -18,10 +19,12 @@ namespace TillWeMeet.API.Controllers
     {
         private readonly IAuthRepository _repo;
         private readonly IConfiguration _config;
-        public AuthController(IAuthRepository repo, IConfiguration config)
+        private readonly IMapper _mapper;
+        public AuthController(IAuthRepository repo, IConfiguration config, IMapper mapper)
         {
-            this._config = config;
-            this._repo = repo;
+            _mapper = mapper;
+            _config = config;
+            _repo = repo;
 
         }
 
@@ -72,9 +75,12 @@ namespace TillWeMeet.API.Controllers
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
+            var user = _mapper.Map<UserForListDto>(userFromRepo);
+
             return Ok(new
             {
-                token = tokenHandler.WriteToken(token)
+                token = tokenHandler.WriteToken(token),
+                user
             });
         }
     }
