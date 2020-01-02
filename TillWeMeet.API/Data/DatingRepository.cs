2dfa586b88bfb6d048f2a1843aa796344a4a6e90
Devin <devdhera@gmail.com>
2020-01-02 21:11:05 +0530

    Added pagination to the users endpoint

--- TillWeMeet.API/Data/DatingRepository.cs
+++ TillWeMeet.API/Data/DatingRepository.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using TillWeMeet.API.Helpers;
 using TillWeMeet.API.Models;
 
 namespace TillWeMeet.API.Data
@@ -44,11 +45,11 @@ namespace TillWeMeet.API.Data
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+            var users = _context.Users.Include(p => p.Photos);
 
-            return users;
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
--- TillWeMeet.API/Data/IDatingRepository.cs
+++ TillWeMeet.API/Data/IDatingRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using TillWeMeet.API.Helpers;
 using TillWeMeet.API.Models;
 
 namespace TillWeMeet.API.Data
@@ -9,7 +10,7 @@ namespace TillWeMeet.API.Data
         void Add<T>(T entity) where T : class;
         void Delete<T>(T entity) where T : class;
         Task<bool> SaveAll();
-        Task<IEnumerable<User>> GetUsers();
+        Task<PagedList<User>> GetUsers(UserParams userParams);
         Task<User> GetUser(int id);
         Task<Photo> GetPhoto(int id);
         Task<Photo> GetMainPhotoForUser(int userId);
