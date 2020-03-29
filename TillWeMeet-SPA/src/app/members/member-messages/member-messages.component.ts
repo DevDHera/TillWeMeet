dcd6074c49243b925664f77898a73727ad24dc4c
Devin <devdhera@gmail.com>
2020-03-29 21:15:28 +0530

    Setup messages thread

--- TillWeMeet-SPA/src/app/members/member-messages/member-messages.component.ts
+++ TillWeMeet-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from '../../_models/message';
+import { UserService } from '../../_services/user.service';
+import { AuthService } from '../../_services/auth.service';
+import { AlertifyService } from '../../_services/alertify.service';
 
 @Component({
   selector: 'app-member-messages',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
+  @Input() recipientId: number;
+  messages: Message[];
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {
+    this.loadMessages();
   }
 
+  loadMessages() {
+    this.userService
+      .getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .subscribe(
+        messages => {
+          this.messages = messages;
+        },
+        error => {
+          this.alertify.error(error);
+        }
+      );
+  }
 }
