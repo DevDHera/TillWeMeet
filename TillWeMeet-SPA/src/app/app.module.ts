d8128179e26a7667e4ba0cbc366751d61a140b10
Devin <devdhera@gmail.com>
2020-03-29 20:32:15 +0530

    Setup message resolver

--- TillWeMeet-SPA/src/app/app.module.ts
+++ TillWeMeet-SPA/src/app/app.module.ts
@@ -38,6 +38,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -94,6 +95,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
     MemberListResolver,
     MemberEditResolver,
     ListsResolver,
+    MessagesResolver,
     { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
   ],
   bootstrap: [AppComponent]
